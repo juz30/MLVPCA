9f330dd2fa34ae1c4ecec9fc3436e85485d41ab2
juz30 <46353091+juz30@users.noreply.github.com>
2019-01-29 22:27:16 -0500

    Delete deflate.R

--- R/deflate.R
+++
@@ -1,9 +0,0 @@
-deflate=function(S, PrevPi){
-    if (!is.null(PrevPi)){
-      p=dim(S)[1]
-      I=diag(p)
-      S=(I-PrevPi)%*%S%*%(I-PrevPi)
-      #S=S-PrevPi%*%S%*%PrevPi
-    }
-  return(S)
-}
